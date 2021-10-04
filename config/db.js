88088655ca0d8878e4a9636def80f792309846c9
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-04 12:23:49 -0400

    Removing ssl

--- config/db.js
+++ config/db.js
@@ -6,9 +6,6 @@ const db = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PWD,
     database: process.env.DB,
-    ssl: {
-        ca: fs.readFileSync('app/global-bundle.pem')
-    }
 });
 
 module.exports = db;
