fd9f8e4e7064141773ff2b2a1b4032e2d23b4462
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-03 23:41:46 -0400

    Removed config folder to see if this works

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-    host: "recipe-book.cg3bjd4dgyaa.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: "DPJrAfoUWR7uRQ.6*TA*NWuW",
-    database: "book"
-});
-
-module.exports = db;
