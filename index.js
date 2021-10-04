5ef0aaf28c37f83a5bc0f588f358f972eb15fe57
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-04 14:02:31 -0400

    Changed method of connecting to db

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
 const express = require('express');
 const db = require('./config/db');
+let mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+let connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+connection.connect();
+console.log("Connected to database successful");
 
-// Route to get all recipes
-app.get("/api/get", (req,res) => {
-    db.query("SELECT * FROM recipes", (err,result) => {
+function performQuery(query, res) {
+    connection.query(query, (err, result) => {
         if(err) {
             console.log(err);
         }
 
         res.send(result);
     });
+}
+
+// Route to get all recipes
+app.get("/api/get", (req,res) => {
+    let q = performQuery("SELECT * FROM recipes", res);
 });
 
 
 // Route to get one recipe
 app.get("/api/get/:id", (req,res) => {
     const id = req.params.id;
-    db.query("SELECT * FROM recipes WHERE id = ?", id, (err,result) => {
-        if(err) {
-            console.log(err);
-        }
-
-        res.send(result);
-    });
+    const query = "SELECT * FROM recipes WHERE id=" + id;
+    let q = performQuery(query, res);
 });
 
 // Get all categories
 app.get("/api/getCategories", (req, res) => {
-    db.query("SELECT * FROM categories", (err, result) => {
-        if(err) {
-            console.log(err);
-        }
-
-        res.send(result);
-    });
+    let q = performQuery("SELECT * FROM categories", res);
 })
 
 // Route to get directions for a recipe
 app.get("/api/getDirections/:id", (req, res) => {
     const id = req.params.id;
-    db.query("SELECT * FROM directions WHERE id = ? ORDER BY step_num", id, (err,result) => {
-        if(err) {
-            console.log(err);
-        }
-
-        res.send(result);
-    });
+    let query = "SELECT * FROM directions WHERE id=" + id + " ORDER BY step_num";
+    let q = performQuery(query, res);
 });
 
 // Route to get ingredients for a recipe
 app.get("/api/getIngredients/:id", (req, res) => {
     const id = req.params.id;
-    const q = ("SELECT * FROM ingredients, rIngredients WHERE rIngredients.id=" + id + " AND rIngredients.ingredient_id=ingredients.id_ingredient;");
-    db.query(q, id, (err,result) => {
-        if(err) {
-            console.log(err);
-        }
-
-        res.send(result);
-    });
+    const query = ("SELECT * FROM ingredients, rIngredients WHERE rIngredients.id=" + id + " AND rIngredients.ingredient_id=ingredients.id_ingredient;");
+    let q = performQuery(query, res);
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+});
