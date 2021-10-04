f1f3fbc2e29f39c7cd9b6e961530d21daca0ac2b
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-04 09:17:06 -0400

    Manually added ca

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     password: process.env.password,
     database: process.env.database,
     ssl: {
-        ca: fs.readFileSync('../app/rds-combined-ca-bundle.pem')
+        ca: fs.readFileSync('../app/rds-ca-2019-root.pem')
     }
 }).then(console.log("Successful connection to database!"));
 
