642456db501d1b97a4319219a797c9724f777971
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-03 23:17:07 -0400

    Fixed which schema to point to

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const db = mysql.createConnection({
     host: "recipe-book.cg3bjd4dgyaa.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "DPJrAfoUWR7uRQ.6*TA*NWuW",
-    database: "sys"
+    database: "book"
 });
 
 module.exports = db;
