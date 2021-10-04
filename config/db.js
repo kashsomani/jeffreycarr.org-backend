1a41183792441d911f78a3248ff872204510de14
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-04 08:40:28 -0400

    Added SSL certificate for AWS RDS

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql');
+
 const db = mysql.createConnection({
     host: process.env.db,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database
+    database: process.env.database,
+    ssl: {
+        ca: '../app/rds-combined-ca-bundle.pem'
+    }
 });
 
 module.exports = db;
