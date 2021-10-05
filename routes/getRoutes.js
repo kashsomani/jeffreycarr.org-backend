92e683b74e132f122d057d87486d156d4eb1edbb
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-05 16:41:52 -0400

    Refactored code

---
+++ routes/getRoutes.js
@@ -0,0 +1,43 @@
+module.exports = (app, pool) => {
+    // Perform general GET query
+    function performQuery(query, res) {
+        pool.query(query, (err, result) => {
+            if(err) {
+                console.log(err);
+            }
+    
+            res.send(result);
+        });
+    }
+
+    // Route to get all recipes
+    app.get("/api/get", (req,res) => {
+        let q = performQuery("SELECT * FROM recipes", res);
+    });
+
+    // Route to get one recipe
+    app.get("/api/get/:id", (req,res) => {
+        const id = req.params.id;
+        const query = "SELECT * FROM recipes WHERE id=" + id;
+        let q = performQuery(query, res);
+    });
+
+    // Get all categories
+    app.get("/api/getCategories", (req, res) => {
+        let q = performQuery("SELECT * FROM categories", res);
+    })
+
+    // Route to get directions for a recipe
+    app.get("/api/getDirections/:id", (req, res) => {
+        const id = req.params.id;
+        let query = "SELECT * FROM directions WHERE id=" + id + " ORDER BY step_num";
+        let q = performQuery(query, res);
+    });
+
+    // Route to get ingredients for a recipe
+    app.get("/api/getIngredients/:id", (req, res) => {
+        const id = req.params.id;
+        const query = ("SELECT * FROM ingredients, rIngredients WHERE rIngredients.id=" + id + " AND rIngredients.ingredient_id=ingredients.id_ingredient;");
+        let q = performQuery(query, res);
+    });
+}
