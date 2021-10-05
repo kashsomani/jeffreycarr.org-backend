edb2354c4fe8baa35f6d2d5cfa6a468510ca7654
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-05 16:56:50 -0400

    Changed method of pooling

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* Express */
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -13,27 +12,10 @@ const mysql = require('mysql');
 /* Other */
 const PORT = process.env.PORT || 5000;
 
-// Get a promise from aws so pool is not created before
-// The certificate authority is received
-const aws = require('./services/aws');
-aws.loadCA().then((data) => {
-    let ca = data.Body.toString('utf-8');
+let pool = mysql.createPool(process.env.CONN);
 
-    let pool = mysql.createPool({
-        connectionLimit: 100,
-        host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PWD,
-        database: process.env.DB,
-        waitForConnections: true,
-        ssl: {
-            ca: ca
-        }
-    });
-
-    require('./routes/getRoutes')(app, pool);
-})
+require('./routes/getRoutes')(app, pool);
 
 app.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
+    console.log("Listening on port: ", PORT);
 });
