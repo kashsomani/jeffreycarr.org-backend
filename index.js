8817f44016921cbf2f76bc749a537f927b13bbcb
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-05 16:41:37 -0400

    Wait for async function to complete before starting code

--- index.js
+++ index.js
@@ -1,72 +1,38 @@
+/* Express */
 const express = require('express');
-let mysql = require('mysql');
-const cors = require('cors');
-
 const app = express();
 app.use(express.json());
-
-// CORS
+/* CORS */
+const cors = require('cors');
 let corsOptions = {
     origin: '*'
 }
 app.use(cors(corsOptions));
-
+/* MySQL */
+const mysql = require('mysql');
+/* Other */
 const PORT = process.env.PORT || 5000;
 
-let pool = mysql.createPool({
-    connectionLimit: 100,
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PWD,
-    database: process.env.DB,
-    waitForConnections: true
-});
+// Get a promise from aws so pool is not created before
+// The certificate authority is received
+const aws = require('./services/aws');
+aws.loadCA().then((data) => {
+    let ca = data.Body.toString('utf-8');
 
-// let connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-// connection.connect();
-console.log("Connection to database successful");
-
-function performQuery(query, res) {
-    pool.query(query, (err, result) => {
-        if(err) {
-            console.log(err);
+    let pool = mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PWD,
+        database: process.env.DB,
+        waitForConnections: true,
+        ssl: {
+            ca: ca
         }
-
-        res.send(result);
     });
-}
 
-// Route to get all recipes
-app.get("/api/get", (req,res) => {
-    let q = performQuery("SELECT * FROM recipes", res);
-});
-
-
-// Route to get one recipe
-app.get("/api/get/:id", (req,res) => {
-    const id = req.params.id;
-    const query = "SELECT * FROM recipes WHERE id=" + id;
-    let q = performQuery(query, res);
-});
-
-// Get all categories
-app.get("/api/getCategories", (req, res) => {
-    let q = performQuery("SELECT * FROM categories", res);
+    require('./routes/getRoutes')(app, pool);
 })
-
-// Route to get directions for a recipe
-app.get("/api/getDirections/:id", (req, res) => {
-    const id = req.params.id;
-    let query = "SELECT * FROM directions WHERE id=" + id + " ORDER BY step_num";
-    let q = performQuery(query, res);
-});
-
-// Route to get ingredients for a recipe
-app.get("/api/getIngredients/:id", (req, res) => {
-    const id = req.params.id;
-    const query = ("SELECT * FROM ingredients, rIngredients WHERE rIngredients.id=" + id + " AND rIngredients.ingredient_id=ingredients.id_ingredient;");
-    let q = performQuery(query, res);
-});
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
